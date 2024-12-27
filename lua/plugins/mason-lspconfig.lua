6ae10fbeb221a26f54c025cba026d0c9d13a136a
tcatarina <tcatarina@proton.me>
2024-12-27 14:20:58 -0300

    Add mason-lspconfig plugin

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,29 @@
+-- get table for LS configs
+local language_server = require("plugin_settings.language_server")
+
+          return {
+	{ -- starts the language servers
+		"williamboman/mason-lspconfig.nvim",
+		lazy = true,
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		opts = {
+			ensure_installed = {
+				"efm",
+				"lua_ls",
+			},
+			automatic_installation = true,
+			handlers = {
+				-- automatically start the language servers
+				language_server.default_setup,
+
+				-- custom settings for specific language servers
+				["efm"] = language_server.efm,
+
+				-- format on save
+				language_server.format_on_save(),
+			},
+		},
+	},
+}
