33b73d20427cb4ca0414c00cdd5f47d0d50c57c5
tcatarina <tcatarina@proton.me>
2024-12-27 17:17:53 -0300

    Add on_attach parameter to lspconfig setup

--- lua/plugin_settings/language_server.lua
+++ lua/plugin_settings/language_server.lua
@@ -1,11 +1,35 @@
 local language_server = {}
 
+local on_attach = function(client, bufnr)
+	local opts = {
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	}
+
+	local keymap = vim.keymap
+
+	keymap.set("n", "<leader>fd", ":Lspsaga finder<CR>", opts)
+	keymap.set("n", "<leader>gd", ":Lspsaga peek_definition<CR>", opts)
+	keymap.set("n", "<leader>gD", ":Lspsaga goto_definition<CR>", opts)
+	keymap.set("n", "<leader>ca", ":Lspsaga code_action<CR>", opts)
+	keymap.set("n", "<leader>rn", ":Lspsaga rename<CR>", opts)
+	keymap.set("n", "<leader>D", ":Lspsaga show_line_diagnostics<CR>", opts)
+	keymap.set("n", "<leader>d", ":Lspsaga show_cursor_diagnostics<CR>", opts)
+	keymap.set("n", "<leader>pd", ":Lspsaga diagnostic_jump_prev<CR>", opts)
+	keymap.set("n", "<leader>nd", ":Lspsaga diagnostic_jump_next<CR>", opts)
+	keymap.set("n", "K", ":Lspsaga hove_doc<CR>", opts)
+end
+
 language_server.default_setup = function(server_name)
-	require("lspconfig")[server_name].setup({})
+	require("lspconfig")[server_name].setup({
+		on_attach = on_attach,
+	})
 end
 
 language_server.efm = function()
 	require("lspconfig").efm.setup({
+		on_attach = on_attach,
 		filetypes = {
 			"lua",
 		},
@@ -29,24 +53,24 @@ language_server.efm = function()
 end
 
 language_server.format_on_save = function()
-  local lsp_fmt_group = vim.api.nvim_create_augroup('LspFormattingGroup', {})
-  vim.api.nvim_create_autocmd('BufWritePost',{
-      group = lsp_fmt_group,
-      callback = function(ev)
-        local efm = vim.lsp.get_clients({
-          name = 'efm',
-          bufnr = ev.buf
-        })
+	local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = lsp_fmt_group,
+		callback = function(ev)
+			local efm = vim.lsp.get_clients({
+				name = "efm",
+				bufnr = ev.buf,
+			})
 
-        if vim.tbl_isempty(efm) then
-          return
-        end
+			if vim.tbl_isempty(efm) then
+				return
+			end
 
-        vim.lsp.buf.format({
-          name = 'efm'
-        })
-      end,
-    })
+			vim.lsp.buf.format({
+				name = "efm",
+			})
+		end,
+	})
 end
 
 return language_server
