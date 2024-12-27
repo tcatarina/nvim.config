9974a15c26082e6bbe58f165d6b752fd7470d58b
tcatarina <tcatarina@proton.me>
2024-12-27 14:20:15 -0300

    Add lazydev plugin

---
+++ lua/plugins/lazydev.lua
@@ -0,0 +1,12 @@
+return {
+  { -- configures luaLS for lua files
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+}
