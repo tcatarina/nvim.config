c167773603300a3f1604f43575fdab1873f85057
tcatarina <tcatarina@proton.me>
2024-12-27 14:04:29 -0300

    Add init file to start lazy, global keymaps, and optional settings

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- Load vim global settings
+require("util.settings")
+
+-- Load vim optional settings
+require("util.options")
+
+-- Load custom keymaps
+require("util.keymaps")
+
+-- Bootstrap Lazy
+require("config.lazy_bootstrap")
+
+-- Configure Lazy
+require("config.lazy_setup")
