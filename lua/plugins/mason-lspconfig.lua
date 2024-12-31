e0610ff4c2d46c00cff556ddf4d90b69b60a5992
tcatarina <tcatarina@proton.me>
2024-12-31 10:44:22 -0300

    Add eslint start function

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -2,27 +2,25 @@
 local language_server = require("plugin_settings.language_server")
 
 return {
-  { -- starts the language servers
-    "williamboman/mason-lspconfig.nvim",
-    lazy = true,
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
-    opts = {
-      ensure_installed = {
-        "efm",
-        "lua_ls",
-      },
-      automatic_installation = true,
-      handlers = {
-        -- automatically start the language servers
-        language_server.default_setup,
+	{ -- starts the language servers
+		"williamboman/mason-lspconfig.nvim",
+		lazy = true,
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+			},
+			automatic_installation = true,
+			handlers = {
+				-- automatically start the language servers
+				language_server.default_setup,
 
-        ["volar"] = language_server.volar,
+				["volar"] = language_server.volar,
 
-        -- format on save
-        language_server.format_on_save(),
-      },
-    },
-  },
+				["eslint"] = language_server.eslint,
+			},
+		},
+	},
 }
