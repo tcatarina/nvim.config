b5fd13a6eca86ad704f7a29101d4778f559c66ed
tcatarina <tcatarina@proton.me>
2024-12-31 10:43:54 -0300

    Add conform and nvim-list plugins

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"mfussenegger/nvim-lint",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+		config = function()
+			local lint = require("lint")
+
+			lint.linters_by_ft = {
+				-- javascript = { "eslint" },
+				-- typescript = { "eslint" },
+				-- vue = { "eslint" },
+				lua = { "luacheck" },
+			}
+
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>l", function()
+				lint.try_lint()
+			end, { desc = "Trigger linting for current file" })
+		end,
+	},
+}
