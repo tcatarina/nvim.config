c296364918186ce48589efd6d48ab3c2a8963805
tcatarina <tcatarina@proton.me>
2024-12-31 10:42:06 -0300

    Remove efm dependency

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,19 +1,15 @@
 return {
-  { -- downloads language servers
-    "williamboman/mason.nvim",
-    dependencies = {
-      "efmls-configs-nvim",
-      "none-ls-autoload.nvim",
-    },
-    lazy = true,
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    },
-  }
+	{ -- downloads language servers
+		"williamboman/mason.nvim",
+		lazy = true,
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
 }
